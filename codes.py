e21570fb95860971e9a48c60bddc9d202636db39
reisferrarig <reisferrarig@gmail.com>
2020-06-16 20:24:55 -0300

    code emoji

--- codes.py
+++ codes.py
@@ -1,3 +1,5 @@
 print(' QUE DELICIA')
 m = input('O miqueias recebeu o que? ')
 print('Isso mesmo miqueias recebeu {}'.format(m))
+import emoji
+print(emoji.emojize('FODASE CARAI : kiss_heart :', use_aliases=True))
